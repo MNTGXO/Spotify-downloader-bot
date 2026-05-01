3fc70a45d8c257b89ee4c3b6610c0a6d61b85d76
Musammil N <mohammedmusammiln@gmail.com>
2026-05-01 08:06:07 +0530

    Fix indentation and formatting in spotify_client.py

--- plugins/spotify_client.py
+++ plugins/spotify_client.py
@@ -7,8 +7,7 @@ SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
 if SPOTIFY_CLIENT_ID and SPOTIFY_CLIENT_SECRET:
     spotdl = Spotdl(
         client_id=SPOTIFY_CLIENT_ID,
-        client_secret=SPOTIFY_CLIENT_SECRET,
-        format="m4a"                        # ← download best m4a, no mp3 conversion
+        client_secret=SPOTIFY_CLIENT_SECRET
     )
 else:
     spotdl = None
